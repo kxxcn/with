bcfd7897b201e9d3462bc164a0819527ab1dbf1d
kxxcn <kxxcn.dev@gmail.com>
2018-08-20 15:02:19 +0900

    Add packages and classes needed for communication.

---
+++ app/src/main/java/dev/kxxcn/app_with/data/DataSource.java
@@ -0,0 +1,7 @@
+package dev.kxxcn.app_with.data;
+
+/**
+ * Created by kxxcn on 2018-08-20.
+ */
+public class DataSource {
+}
---
+++ app/src/main/java/dev/kxxcn/app_with/data/remote/APIPersistence.java
@@ -0,0 +1,7 @@
+package dev.kxxcn.app_with.data.remote;
+
+/**
+ * Created by kxxcn on 2018-08-20.
+ */
+public class APIPersistence {
+}
