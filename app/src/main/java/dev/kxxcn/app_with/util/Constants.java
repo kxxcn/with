faefb8b47c4cf6016ae91867f7d5c7bdfa61319f
kxxcn <kxxcn.dev@gmail.com>
2018-08-23 12:24:47 +0900

    Add constants.

--- app/src/main/java/dev/kxxcn/app_with/util/Constants.java
+++ app/src/main/java/dev/kxxcn/app_with/util/Constants.java
@@ -1,9 +1,20 @@
 package dev.kxxcn.app_with.util;
 
+import android.Manifest;
+
 /**
  * Created by kxxcn on 2018-08-13.
  */
 public class Constants {
 	public static final String TAG = "kxxcn";
 	public static final String SIMPLE_DATE_FORMAT = "yyyy-MM-dd";
+	public static final String READ_EXTERNAL_STORAGE = Manifest.permission.READ_EXTERNAL_STORAGE;
+
+	public enum TypeFilter {
+		PRIMARY,
+		GALLERY,
+		FONT,
+		COLOR,
+		RESET
+	}
 }
