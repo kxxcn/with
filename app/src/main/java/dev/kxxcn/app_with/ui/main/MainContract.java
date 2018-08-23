b8fb854dd618c2370b8d49457e3f46e6cb8d2354
kxxcn <kxxcn.dev@gmail.com>
2018-08-23 12:21:48 +0900

    Remove an interface "OnClickCallback".

--- app/src/main/java/dev/kxxcn/app_with/ui/main/MainContract.java
+++ app/src/main/java/dev/kxxcn/app_with/ui/main/MainContract.java
@@ -1,14 +1,12 @@
 package dev.kxxcn.app_with.ui.main;
 
+import dev.kxxcn.app_with.util.Constants;
+
 /**
  * Created by kxxcn on 2018-08-14.
  */
 public interface MainContract {
-	interface OnClickCallback {
-		void onClickCallback();
-	}
-
 	interface OnItemClickListener {
-		void onItemClickListener(int position, int type);
+		void onItemClickListener(int position, Constants.TypeFilter typeFilter);
 	}
 }
