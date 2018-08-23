cb8f56a31d428fd55fc5dc8ac970a05070293ed3
kxxcn <kxxcn.dev@gmail.com>
2018-08-23 12:22:23 +0900

    Add a screen to splash.

---
+++ app/src/main/java/dev/kxxcn/app_with/ui/splash/SplashActivity.java
@@ -0,0 +1,20 @@
+package dev.kxxcn.app_with.ui.splash;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import dev.kxxcn.app_with.R;
+
+/**
+ * Created by kxxcn on 2018-08-22.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+	}
+
+}
