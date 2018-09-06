2ef683ca1e86371be2157f08537616e1c6094ee4
kxxcn <kxxcn.dev@gmail.com>
2018-09-06 15:06:06 +0900

    Add variables related to API.

--- app/src/main/java/dev/kxxcn/app_with/data/remote/APIPersistence.java
+++ app/src/main/java/dev/kxxcn/app_with/data/remote/APIPersistence.java
@@ -4,4 +4,12 @@ package dev.kxxcn.app_with.data.remote;
  * Created by kxxcn on 2018-08-20.
  */
 public class APIPersistence {
+	public static final String SERVER_URL = "http://183.103.101.187:80/with/";
+	public static final String GSON_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+
+	public static final String GET_KEY = "getPairingKey.php";
+	public static final String AUTHENTICATE_KEY = "authenticateKey.php";
+	public static final String IS_REGISTERED_USER = "isRegisteredUser.php";
+
+	public static final String THROWABLE_NETWORK = "Network Error";
 }
