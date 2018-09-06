4ed480279ba179fc972f3d2620e502a9b6b69694
kxxcn <kxxcn.dev@gmail.com>
2018-09-06 15:07:09 +0900

    Add an abstract interface.

--- app/src/main/java/dev/kxxcn/app_with/data/DataSource.java
+++ app/src/main/java/dev/kxxcn/app_with/data/DataSource.java
@@ -3,5 +3,33 @@ package dev.kxxcn.app_with.data;
 /**
  * Created by kxxcn on 2018-08-20.
  */
-public class DataSource {
+public abstract class DataSource {
+	public interface GetCommonCallback {
+		void onSuccess();
+
+		void onFailure(Throwable throwable);
+	}
+
+	public interface GetKeyCallback {
+		void onSuccess(String key);
+
+		void onFailure(Throwable throwable);
+
+		void onNetworkFailure();
+	}
+
+	public interface GetResultCallback {
+		void onSuccess();
+
+		void onFailure(Throwable throwable);
+
+		void onRequestFailure(String stat);
+	}
+
+
+	public abstract void onCreatePairingKey(GetKeyCallback callback, String uniqueIdentifier);
+
+	public abstract void onAuthenticate(GetResultCallback callback, String uniqueIdentifier, String key, int gender);
+
+	public abstract void isRegisteredUser(GetResultCallback callback, String uniqueIdentifier);
 }
