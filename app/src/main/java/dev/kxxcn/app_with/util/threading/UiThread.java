3269783abc61bedd5f81baa9d0ab15b48c5139e3
kxxcn <kxxcn.dev@gmail.com>
2018-09-06 15:02:46 +0900

    Add an util "UiThread".

---
+++ app/src/main/java/dev/kxxcn/app_with/util/threading/UiThread.java
@@ -0,0 +1,32 @@
+package dev.kxxcn.app_with.util.threading;
+
+import android.os.Handler;
+import android.os.Looper;
+
+/**
+ * Created by kxxcn on 2018-08-29.
+ */
+public class UiThread {
+	private static UiThread uiThread;
+
+	private Handler handler;
+
+	private UiThread() {
+		handler = new Handler(Looper.getMainLooper());
+	}
+
+	public static synchronized UiThread getInstance() {
+		if (uiThread == null) {
+			uiThread = new UiThread();
+		}
+		return uiThread;
+	}
+
+	public void post(Runnable runnable) {
+		handler.post(runnable);
+	}
+
+	public void postDelayed(Runnable runnable, long delay) {
+		handler.postDelayed(runnable, delay);
+	}
+}
