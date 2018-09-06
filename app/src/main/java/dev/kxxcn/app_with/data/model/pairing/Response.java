abfe20aa243e1356826655dc09eb38bcf8a40599
kxxcn <kxxcn.dev@gmail.com>
2018-09-06 15:10:51 +0900

    Add a model that receives the response of api service.

---
+++ app/src/main/java/dev/kxxcn/app_with/data/model/pairing/Response.java
@@ -0,0 +1,14 @@
+package dev.kxxcn.app_with.data.model.pairing;
+
+/**
+ * Created by kxxcn on 2018-09-06.
+ */
+public class Response {
+
+	private String key;
+
+	public String getKey() {
+		return key;
+	}
+
+}
---
+++ app/src/main/java/dev/kxxcn/app_with/data/model/result/Response.java
@@ -0,0 +1,19 @@
+package dev.kxxcn.app_with.data.model.result;
+
+/**
+ * Created by kxxcn on 2018-09-06.
+ */
+public class Response {
+
+	private int rc;
+	private String stat;
+
+	public int getRc() {
+		return rc;
+	}
+
+	public String getStat() {
+		return stat;
+	}
+
+}
