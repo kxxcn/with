8fac4a1fde66cde57638cf2ce281ae50ff1b8f84
kxxcn <kxxcn.dev@gmail.com>
2018-09-13 16:56:06 +0900

    Add a parameter "OnPermissionListener"

--- app/src/main/java/dev/kxxcn/app_with/util/PermissionUtils.java
+++ app/src/main/java/dev/kxxcn/app_with/util/PermissionUtils.java
@@ -10,23 +10,24 @@ import com.gun0912.tedpermission.TedPermission;
 import java.util.ArrayList;
 
 import dev.kxxcn.app_with.R;
+import dev.kxxcn.app_with.ui.splash.SplashContract;
 
 /**
  * Created by kxxcn on 2018-08-22.
  */
 public class PermissionUtils {
-	public static void authorization(Activity activity, String... permission) {
+	public static void authorization(Activity activity, SplashContract.OnPermissionListener listener, String... permission) {
 		int permissionCheck = ContextCompat.checkSelfPermission(activity, permission[0]);
 		if (permissionCheck == PackageManager.PERMISSION_DENIED) {
 			PermissionListener permissionListener = new PermissionListener() {
 				@Override
 				public void onPermissionGranted() {
-					// ▼ 권한 획득 성공
+					listener.onGranted();
 				}
 
 				@Override
 				public void onPermissionDenied(ArrayList<String> deniedPermissions) {
-					// ▼ 권한 획득 실패
+					listener.onDenied();
 				}
 			};
 
@@ -36,6 +37,8 @@ public class PermissionUtils {
 					.setGotoSettingButtonText(activity.getString(R.string.system_setting))
 					.setPermissions(permission)
 					.check();
+		} else {
+			listener.onGranted();
 		}
 	}
 }
