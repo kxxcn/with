4c93b501739956b76c21562955224475c62c7e94
kxxcn <kxxcn.dev@gmail.com>
2018-09-13 16:55:15 +0900

    Add constants.

--- app/src/main/java/dev/kxxcn/app_with/util/Constants.java
+++ app/src/main/java/dev/kxxcn/app_with/util/Constants.java
@@ -9,8 +9,11 @@ public class Constants {
 	public static final String TAG = "kxxcn";
 	public static final String SIMPLE_DATE_FORMAT = "yyyy-MM-dd";
 	public static final String READ_EXTERNAL_STORAGE = Manifest.permission.READ_EXTERNAL_STORAGE;
+	public static final String GENDER = "GENDER";
+	public static final String IDENTIFIER = "IDENTIFIER";
 
-	public static final int DELAY = 2000;
+	public static final int DELAY_NETWORK = 2000;
+	public static final int DELAY_TOAST = 2000;
 
 	public enum TypeFilter {
 		PRIMARY,
