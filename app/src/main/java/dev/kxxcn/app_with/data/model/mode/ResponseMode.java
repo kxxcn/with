a3f30c383418878535b84e6ba3ef8e522e5d892d
kxxcn <kxxcn.dev@gmail.com>
2018-12-06 16:05:54 +0900

    Modified the model name.

---
+++ app/src/main/java/dev/kxxcn/app_with/data/model/mode/ResponseMode.java
@@ -0,0 +1,14 @@
+package dev.kxxcn.app_with.data.model.mode;
+
+/**
+ * Created by kxxcn on 2018-12-06.
+ */
+public class ResponseMode {
+
+	private String lover;
+
+	public String getLover() {
+		return lover;
+	}
+
+}
