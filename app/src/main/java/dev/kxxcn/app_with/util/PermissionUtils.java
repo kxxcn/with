4a885ef6a7732ce0c3ec1451b261621aaf43bec1
kxxcn <kxxcn.dev@gmail.com>
2019-01-03 13:43:12 +0900

    Modified the logic to request permissions.

--- app/src/main/java/dev/kxxcn/app_with/util/PermissionUtils.java
+++ app/src/main/java/dev/kxxcn/app_with/util/PermissionUtils.java
@@ -16,29 +16,31 @@ import dev.kxxcn.app_with.ui.BasePresenter;
  * Created by kxxcn on 2018-08-22.
  */
 public class PermissionUtils {
-	public static void authorization(Activity activity, BasePresenter.OnPermissionListener listener, String... permission) {
-		int permissionCheck = ContextCompat.checkSelfPermission(activity, permission[0]);
-		if (permissionCheck == PackageManager.PERMISSION_DENIED) {
-			PermissionListener permissionListener = new PermissionListener() {
-				@Override
-				public void onPermissionGranted() {
-					listener.onGranted();
-				}
+	public static void authorization(Activity activity, BasePresenter.OnPermissionListener listener, String... permissions) {
+		for (String permission : permissions) {
+			int permissionCheck = ContextCompat.checkSelfPermission(activity, permission);
+			if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+				PermissionListener permissionListener = new PermissionListener() {
+					@Override
+					public void onPermissionGranted() {
+						listener.onGranted();
+					}
 
-				@Override
-				public void onPermissionDenied(ArrayList<String> deniedPermissions) {
-					listener.onDenied();
-				}
-			};
+					@Override
+					public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+						listener.onDenied();
+					}
+				};
 
-			TedPermission.with(activity)
-					.setPermissionListener(permissionListener)
-					.setDeniedMessage(activity.getString(R.string.system_denied_permission))
-					.setGotoSettingButtonText(activity.getString(R.string.system_setting))
-					.setPermissions(permission)
-					.check();
-		} else {
-			listener.onGranted();
+				TedPermission.with(activity)
+						.setPermissionListener(permissionListener)
+						.setDeniedMessage(activity.getString(R.string.system_denied_permission))
+						.setGotoSettingButtonText(activity.getString(R.string.system_setting))
+						.setPermissions(permission)
+						.check();
+			} else {
+				listener.onGranted();
+			}
 		}
 	}
 }
