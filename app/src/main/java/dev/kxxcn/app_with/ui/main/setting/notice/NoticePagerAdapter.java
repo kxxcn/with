6dd49e4e9d63657bcc16731b8e30903eb9abc575
kxxcn <kxxcn.dev@gmail.com>
2019-03-16 09:57:32 +0900

    Fixed an error "NullPointerException".

--- app/src/main/java/dev/kxxcn/app_with/ui/main/setting/notice/NoticePagerAdapter.java
+++ app/src/main/java/dev/kxxcn/app_with/ui/main/setting/notice/NoticePagerAdapter.java
@@ -1,8 +1,10 @@
 package dev.kxxcn.app_with.ui.main.setting.notice;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,37 +18,43 @@ import dev.kxxcn.app_with.ui.main.setting.notice.subject.SubjectFragment;
  */
 public class NoticePagerAdapter extends FragmentStatePagerAdapter {
 
-	private static final int COUNT = 2;
+    private static final int COUNT = 2;
 
-	public static final int SUBJECT = 0;
-	public static final int CONTENT = 1;
+    public static final int SUBJECT = 0;
+    public static final int CONTENT = 1;
 
-	private ArrayList<Notice> noticeList = new ArrayList<>(0);
+    private ArrayList<Notice> noticeList = new ArrayList<>(0);
 
-	private NoticeContract.OnNoticeClickListener mListener;
+    private NoticeContract.OnNoticeClickListener mListener;
 
-	public NoticePagerAdapter(FragmentManager fm, List<Notice> noticeList, NoticeContract.OnNoticeClickListener listener) {
-		super(fm);
-		this.noticeList.addAll(noticeList);
-		this.mListener = listener;
-	}
+    public NoticePagerAdapter(FragmentManager fm, List<Notice> noticeList, NoticeContract.OnNoticeClickListener listener) {
+        super(fm);
+        this.noticeList.addAll(noticeList);
+        this.mListener = listener;
+    }
 
-	@Override
-	public Fragment getItem(int position) {
-		switch (position) {
-			case SUBJECT:
-				SubjectFragment subjectFragment = SubjectFragment.newInstance(noticeList);
-				subjectFragment.setOnNoticeClickListener(mListener);
-				return subjectFragment;
-			case CONTENT:
-				return ContentFragment.newInstance(noticeList);
-		}
-		return null;
-	}
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case SUBJECT:
+                SubjectFragment subjectFragment = SubjectFragment.newInstance(noticeList);
+                subjectFragment.setOnNoticeClickListener(mListener);
+                return subjectFragment;
+            case CONTENT:
+                return ContentFragment.newInstance(noticeList);
+        }
+        return null;
+    }
 
-	@Override
-	public int getCount() {
-		return COUNT;
-	}
+    @Override
+    public int getCount() {
+        return COUNT;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        return super.instantiateItem(container, position);
+    }
 
 }
