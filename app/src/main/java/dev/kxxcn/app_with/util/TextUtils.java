086149735ed51b0da2ead65f8de67ab6e1543542
kxxcn <kxxcn.dev@gmail.com>
2019-03-27 13:13:01 +0900

    Added the utils to check whether string is null or empty.

---
+++ app/src/main/java/dev/kxxcn/app_with/util/TextUtils.java
@@ -0,0 +1,10 @@
+package dev.kxxcn.app_with.util;
+
+/**
+ * Created by kxxcn on 2019-03-27.
+ */
+public class TextUtils {
+	public static boolean isNullOrEmpty(CharSequence str) {
+		return str == null || str.length() == 0;
+	}
+}
