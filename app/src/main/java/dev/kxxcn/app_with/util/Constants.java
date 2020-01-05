309382b14b15e713542bd774451bbdf3a9b8bd48
kxxcn <kxxcn.dev@gmail.com>
2020-01-05 13:45:11 +0900

    배경 색상 6종 추가

--- app/src/main/java/dev/kxxcn/app_with/util/Constants.java
+++ app/src/main/java/dev/kxxcn/app_with/util/Constants.java
@@ -45,7 +45,9 @@ public class Constants {
 
     public static final int[] DAY_COLORS = {R.color.day_background0, R.color.day_background1, R.color.day_background2, R.color.day_background3,
             R.color.day_background4, R.color.day_background5, R.color.day_background6, R.color.day_background7, R.color.day_background8,
-            R.color.day_background9, R.color.day_background10, R.color.day_background11};
+            R.color.day_background9, R.color.day_background10, R.color.day_background11, R.color.day_background12, R.color.day_background13,
+            R.color.day_background14, R.color.day_background15, R.color.day_background16, R.color.day_background17};
+
 
     public static final int[] COLOR_DEFAULT = {R.drawable.color_default};
 
